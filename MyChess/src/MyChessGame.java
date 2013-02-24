0a3315e56c14c6b77a4b9e96a020482e1dff63ba
Nitin Jain <jain.nitin2050@gmail.com>
2013-02-24 14:43:35 -0800

    Initial commit
    Testing

---
+++ MyChess/src/MyChessGame.java
@@ -0,0 +1,15 @@
+
+class Version {
+	private static String versionNo = "1.0";
+
+	public static String getVersionNo()
+	{
+		return versionNo;
+	}
+
+	public static void printVersionNo()
+	{
+		System.out.println("Current VersionNo. is:" + versionNo);
+	}
+
+}
